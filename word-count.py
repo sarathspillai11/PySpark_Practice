31db8327317fb6bb64af8ba83170ef52b8fa9c47
sarathspillai11 <37377707+sarathspillai11@users.noreply.github.com>
2021-06-04 15:43:07 +0530

    Adding all practice problems

---
+++ word-count.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf = conf)
+
+input = sc.textFile("file:///C:/Users/Sarath S Pillai/sparkcourse/book.txt")
+print(type(input))
+words = input.flatMap(lambda x: x.split())
+print(type(words))
+wordCounts = words.countByValue()
+
+for word, count in wordCounts.items():
+    cleanWord = word.encode('ascii', 'ignore')
+    if (cleanWord):
+        print(cleanWord.decode() + " " + str(count))
